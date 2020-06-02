09c0c04e8ce70c5473edd77ebc2bf13679d179ca
Dzmitry Tsebruk <mikhama@tut.by>
2020-06-02 23:04:14 +0300

    feat: create basic project structure

---
+++ src/app/app.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const App = () => (
+  <header className="header">
+    <h1 className="header__title">mikhama.art</h1>
+    <article className="header__about">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse at rhoncus justo.
+      </p>
+      <p>
+        Quisque efficitur lacus in auctor imperdiet. Aenean sollicitudin semper nisi nec malesuada.
+      </p>
+      <p>
+        Aenean quam augue, varius et congue in, aliquet quis turpis. Suspendisse sed ultricies
+        tellus, et consectetur enim. Aliquam commodo nulla ante, et pretium risus interdum eget.
+        Nam dapibus risus lectus, ac efficitur ligula convallis sit amet.
+      </p>
+    </article>
+  </header>
+);
+
+export default App;
---
+++ src/app/index.js
@@ -0,0 +1,5 @@
+import App from './app';
+
+import './app.scss';
+
+export default App;
